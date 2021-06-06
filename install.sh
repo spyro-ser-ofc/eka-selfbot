bae8544d990d990fb6a0e7dbac9664d4c8329684
エカダヌアルタ <80229956+ekadanuarta@users.noreply.github.com>
2021-06-06 15:00:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ apt-get install -y nodejs libwebp ffmpeg wget tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "[*] Sudah kelar ngab sekarang ketik \"npm start\" untuk menjalankan bot "
+echo "[*] Sudah kelar ngab di atas eror biarkan saja, ketik aja \"npm start\" untuk menjalankan bot "
